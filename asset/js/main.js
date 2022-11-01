fa8b74bc673611d9b58f219d8fdbff2dbc0c96b6
Meera <meera.sharma@ladybirdweb.com>
2022-11-01 11:51:58 +0530

    blank push

--- asset/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(window).bind('scroll', function() {
-        var navHeight = $("#box1").height();
-        ($(window).scrollTop() > navHeight) ? $('nav').addClass('goToTop') : $('nav').removeClass('goToTop');
-    });
-});
--- resources/views/footer.blade.php
+++
@@ -1,6 +0,0 @@
-	<footer class="main-footer">
-      <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.7
-      </div>
-      <strong>Copyright © 2016  <a href="#" target="_blank">Ladybird Web Solution</a>.</strong> All right Reserved. Powered by <a href="http://www.faveohelpdesk.com/" target="_blank">Faveo</a>
-    </footer>
