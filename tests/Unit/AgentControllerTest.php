06ccedde70f3ef995e72f13ea2487511531b67c7
Meera <meera.sharma@ladybirdweb.com>
2023-01-05 00:39:56 +0530

    agent test cases

---
+++ tests/Unit/AgentControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Department;
+use App\Models\User;
+use Tests\DBTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AgentControllerTest extends DBTestCase
+{
+    use RefreshDatabase;
+
+    public function test_addAgents()
+    {
+        $this->getLoggedInUserForWeb('admin');
+        $departmentId =  Department::factory()->create()->id;
+        $response = $this->post('addAgents', ['username' => 'xyz', 'email' => 'xyz@test.com', 'password' => 'xyz@123', 'confirmpassword' => 'xyz@123', 'departments' => $departmentId]);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', ['name' => 'xyz', 'email' => 'xyz@test.com']);
+        $userId = User::where('email', 'xyz@test.com')->pluck('id')->toArray();
+        $this->assertDatabaseHas('user_assign_departments', ['user_id' => $userId[0], 'department_id' => $departmentId]);
+    }
+
+    public function test_deleteAgent()
+    {
+        $this->getLoggedInUserForWeb('admin');
+        $departmentId =  Department::factory()->create()->id;
+        $response = $this->post('addAgents', ['username' => 'xyz', 'email' => 'xyz@test.com', 'password' => 'xyz@123', 'confirmpassword' => 'xyz@123', 'departments' => $departmentId]);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', ['name' => 'xyz', 'email' => 'xyz@test.com']);
+        $userId = User::where('email', 'xyz@test.com')->pluck('id')->toArray();
+        $response = $this->get('deleteUser/'.$userId[0]);
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('users',['name'=>'xyz']);
+    }
+
+    public function test_editAgent()
+    {
+        $this->getLoggedInUserForWeb('admin');
+        $departmentId =  Department::factory()->create()->id;
+        $response = $this->post('addAgents', ['username' => 'xyz', 'email' => 'xyz@test.com', 'password' => 'xyz@123', 'confirmpassword' => 'xyz@123', 'departments' => $departmentId]);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', ['name' => 'xyz', 'email' => 'xyz@test.com']);
+        $userId = User::where('email', 'xyz@test.com')->pluck('id')->toArray();
+        $response = $this->get('editUser/'.$userId[0]);
+        $user = json_decode($response->getContent())->data;
+        $this->assertEquals($user[0]->name,'xyz');
+        $response = $this->post('updateAgents', ['username' => 'abc', 'department_name' => $departmentId, 'id'=> $userId[0]]);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', ['name' => 'abc', 'email' => 'xyz@test.com']);
+    }
+
+}
--- tests/Unit/DepartmentControllerTest.php
+++ tests/Unit/DepartmentControllerTest.php
@@ -2,9 +2,7 @@
 
 namespace Tests\Unit;
 
-//use PHPUnit\Framework\TestCase;
 use App\Models\Department;
-use App\Models\User;
 use Tests\DBTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -12,35 +10,21 @@ class DepartmentControllerTest extends DBTestCase
 {
     use RefreshDatabase;
 
-    public function test_addDeppartment() {
-//       $userId =  User::factory()->create(['role' => 'admin'])->id;
-//     $user =  User::find($userId);
-//      $response = $this->call('post','checklogin', ['email'=>$user['email'],'password' => 'password']);
-//       $response->assertStatus(200);
+    public function test_addDeppartment()
+    {
         $this->getLoggedInUserForWeb('admin');
-
         $response = $this->post('addDepartment', ['name' => 'hii', 'description' => 'hii']);
         $response->assertStatus(200);
         $this->assertDatabaseHas('departments', ['name' => 'hii', 'description' => 'hii']);
     }
 
-
-    public function test_deleteDepartment() {
-//        $userId =  User::factory()->create(['role' => 'admin'])->id;
-//        $user =  User::find($userId);
-//        $response = $this->call('post','checklogin', ['email'=>$user['email'],'password' => 'password']);
-//        $response->assertStatus(200);
+    public function test_deleteDepartment()
+    {
         $this->getLoggedInUserForWeb('admin');
-
-//        $departmentId = Department::factory()->create()->id;
-//        $department = Department::find($departmentId);
-
         $response = $this->post('addDepartment', ['name' => 'sales 8', 'description' => 'sales']);
         $response->assertStatus(200);
         $this->assertDatabaseHas('departments', ['name' => 'sales 8', 'description' => 'sales']);
-
         $departmentId = Department::where('name','sales 8')->pluck('id')->toArray();
-//        dd($departmentId[0]);
         $response = $this->get('deleteDepartment/'.$departmentId[0]);
         $response->assertStatus(200);
         $this->assertDatabaseMissing('departments',['name'=>'sales 8']);
@@ -48,31 +32,17 @@ class DepartmentControllerTest extends DBTestCase
 
     public function test_editDepartmnt()
     {
-//        $userId =  User::factory()->create(['role' => 'admin'])->id;
-//        $user =  User::find($userId);
-//        $response = $this->call('post','checklogin', ['email'=>$user['email'],'password' => 'password']);
-//        $response->assertStatus(200);
         $this->getLoggedInUserForWeb('admin');
-
         $response = $this->post('addDepartment', ['name' => 'sales 17', 'description' => 'sales desc']);
         $response->assertStatus(200);
         $this->assertDatabaseHas('departments', ['name' => 'sales 17', 'description' => 'sales desc']);
-
         $departmentId = Department::where('name','sales 17')->pluck('id')->toArray();
         $response = $this->get('edit/'.$departmentId[0]);
         $department = json_decode($response->getContent())->data;
         $this->assertEquals($department->name,'sales 17');
-
         $response = $this->post('editDepartment', ['name' => 'updated department', 'description' => 'sales desc']);
         $response->assertStatus(200);
         $this->assertDatabaseHas('departments', ['name' => 'updated department', 'description' => 'sales desc']);
-
-
-
-
-
-
     }
-
 
 }
